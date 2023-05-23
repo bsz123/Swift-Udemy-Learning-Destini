0b31c2ea53c0533600cd81358b0ebb52daf7670d
bsz123 <ben.zuke@amway.com>
2023-05-23 12:49:10 -0400

    fix

--- Destini-iOS13/Model/StoryBrain.swift
+++ Destini-iOS13/Model/StoryBrain.swift
@@ -13,7 +13,7 @@ struct StoryBrain {
     
     let stories = [
         Story(title: "You see a fork in the road", choice1: "Take a left", choice2: "Take a right"),
-        Story(title: "You see a tiger", choice1: "Shout for help.", choice2: "Play dead")
+        Story(title: "You see a tiger", choice1: "Shout for help.", choice2: "Play dead"),
         Story(title: "You see a chest", choice1: "Open it", choice2: "Check for traps")
         
         ]
