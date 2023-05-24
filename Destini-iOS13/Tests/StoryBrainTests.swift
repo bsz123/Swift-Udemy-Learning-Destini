516b26795f0d9265fc4894f2b06e895e6f55625d
bsz123 <ben.zuke@amway.com>
2023-05-24 12:58:15 -0400

    Unit tests cleaned

--- Destini-iOS13/Tests/StoryBrainTests.swift
+++ Destini-iOS13/Tests/StoryBrainTests.swift
@@ -12,28 +12,7 @@ import XCTest
 final class StoryBrainTests: XCTestCase {
 
     let storyBrain = StoryBrain()
-    
-    override func setUpWithError() throws {
-        // Create a story
-        
-        
-    }
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        
-
-        
-        
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
     
     // check story step
     func testStep() throws {
@@ -67,13 +46,6 @@ final class StoryBrainTests: XCTestCase {
         XCTAssert(story2.choice2.hasPrefix("Wait, I know how to"))
         
         
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
     }
 
 }
